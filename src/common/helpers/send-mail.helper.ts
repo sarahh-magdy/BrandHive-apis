a87e40c6a41320ee58c82fc4d17f62e6a9f179d3
sarah <magdysara87@gmail.com>
2026-04-17 14:56:07 +0200

    first commit

--- src/common/helpers/send-mail.helper.ts
+++ src/common/helpers/send-mail.helper.ts
@@ -1,17 +1,21 @@
 import * as nodemailer from 'nodemailer';
 
 export async function sendMail(mailOptions: nodemailer.SendMailOptions) {
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',  
-    port: 587,               
-    secure: false,          
-    auth: {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
-});
-        await transporter.sendMail(mailOptions);
-    }
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+
+    await transporter.verify(); // مهم جدًا للتأكد من الاتصال
+
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.log('EMAIL ERROR:', error);
+  }
+}
