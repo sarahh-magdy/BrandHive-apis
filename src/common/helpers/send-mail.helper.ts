eb449cef65cf2a6c34225568a043b6290fe5a847
sarah <magdysara87@gmail.com>
2026-04-17 15:22:01 +0200

    first commit

--- src/common/helpers/send-mail.helper.ts
+++ src/common/helpers/send-mail.helper.ts
@@ -2,10 +2,12 @@ import * as nodemailer from 'nodemailer';
 
 export async function sendMail(mailOptions: nodemailer.SendMailOptions) {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD, 
+      pass: process.env.EMAIL_PASSWORD,
     },
   });
 
