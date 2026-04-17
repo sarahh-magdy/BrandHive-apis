a556d51cc223e6eaebb84206302a7030f532b35c
sarah <magdysara87@gmail.com>
2026-04-17 15:05:00 +0200

    first commit

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import dns from 'dns';
 
 async function bootstrap() {
+  dns.setDefaultResultOrder('ipv4first');
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // تفعيل الـ CORS عشان بتوع الفلاتر والفرونت اند ميعملوش Errors
   app.enableCors();
 
   app.useGlobalPipes(
@@ -22,10 +24,10 @@ async function bootstrap() {
     prefix: '/uploads/',
   });
 
-  // تحديد البورت والـ Host
   const port = process.env.PORT ?? 3000;
   await app.listen(port, '0.0.0.0');
-  
+
   console.log(`Server running on: http://0.0.0.0:${port}`);
 }
+
 bootstrap();
