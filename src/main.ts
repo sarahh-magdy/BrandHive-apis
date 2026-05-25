7c7c8b8bba1b5ea31ade55c012ef5eac80277097
sarah <magdysara87@gmail.com>
2026-05-25 18:23:45 +0300

    updated

--- src/main.ts
+++ src/main.ts
@@ -40,5 +40,7 @@ async function bootstrap() {
   await app.listen(port, '0.0.0.0');
 
   console.log(`Server running on port: ${port}`);
+  console.log('DB_URL =', process.env.DB_URL);
+  
 }
 bootstrap();
